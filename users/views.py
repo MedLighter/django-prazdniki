bef60f1650211881b4b55d0914ff0676341f043e
MedLighter <ilyaudalov2004@yandex.ru>
2023-04-23 21:52:24 +0300

    add forms for login and registration e.t.c.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm, UserRegistrationForm
 
-# Create your views here.
+
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+    context={
+        'form': form,
+        'title': 'Login'
+    }
+    return render(request, 'users/login.html', context)
+
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form,
+        'title': 'Registration'
+    }
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    return render(request, 'users/profile.html')
